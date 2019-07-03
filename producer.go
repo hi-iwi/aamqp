fac2749d10876d20d5c5b14a2ba9e454af329a7a
luexu <Aario@luexu.com>
2019-07-03 15:19:17 +0800

    publish connection with connection pool

--- producer.go
+++ producer.go
@@ -3,8 +3,15 @@ package aamqp
 import (
 	"encoding/json"
 	"errors"
+	"log"
+	"sync"
 
 	"github.com/streadway/amqp"
+)
+
+var (
+	connPool *sync.Pool
+	connOnce sync.Once
 )
 
 type Producer struct {
@@ -15,6 +22,19 @@ type Producer struct {
 
 	uri      string
 	exchange Exchange
+}
+type Conn struct {
+	longConnection bool
+	connection     *amqp.Connection
+}
+
+// Close close short connection
+func (c *Conn) Close() {
+	if c.longConnection {
+		connPool.Put(c.connection)
+	} else {
+		c.connection.Close()
+	}
 }
 
 func NewProducer(uri string, ex Exchange, ccs ...ConnectionConfig) *Producer {
@@ -29,6 +49,52 @@ func NewProducer(uri string, ex Exchange, ccs ...ConnectionConfig) *Producer {
 		exchange: ex,
 	}
 	return producer
+}
+
+func (p *Producer) connect() (*amqp.Connection, error) {
+	ac := amqp.Config{
+		Properties: amqp.Table{
+			"product": defaultProduct,
+			"version": defaultVersion,
+		},
+		Heartbeat: p.cc.Heartbeat,
+		Locale:    p.cc.Locale,
+		Dial:      amqp.DefaultDial(p.cc.Timeout),
+	}
+	return amqp.DialConfig(p.uri, ac)
+
+}
+
+func (p *Producer) ConnectionPool() (*Conn, error) {
+	connOnce.Do(func() {
+		connPool = &sync.Pool{
+			New: func() interface{} {
+				c, err := p.connect()
+				if err != nil {
+					log.Println("amqp connection error: " + err.Error())
+				}
+				return c
+			},
+		}
+	})
+
+	c := connPool.Get().(*amqp.Connection)
+	if c == nil || c.IsClosed() {
+		// using short connection
+		c, err := p.connect()
+		if err != nil {
+			return nil, err
+		}
+		return &Conn{
+			longConnection: false,
+			connection:     c,
+		}, nil
+	}
+
+	return &Conn{
+		longConnection: true,
+		connection:     c,
+	}, nil
 }
 
 // SimplePub msg[, key[, exchange]]
@@ -74,22 +140,13 @@ func (p *Producer) SimplePub(msg interface{}, args ...string) error {
 // @param confirm: Reliable publisher confirms require confirm.select support from the connection.
 //        e.g. func confirmOne(confirms <-chan amqp.Confirmation){if confirmed := <-confirms; confirmed.Ack{OK} else {FAIL}}
 func (p *Producer) Pub(exchange, key string, mandatory, immediate bool, msg amqp.Publishing, confirming func(<-chan amqp.Confirmation)) error {
-	ac := amqp.Config{
-		Properties: amqp.Table{
-			"product": defaultProduct,
-			"version": defaultVersion,
-		},
-		Heartbeat: p.cc.Heartbeat,
-		Locale:    p.cc.Locale,
-		Dial:      amqp.DefaultDial(p.cc.Timeout),
-	}
-	conn, err := amqp.DialConfig(p.uri, ac)
+	c, err := p.ConnectionPool()
 	if err != nil {
 		return errors.New("failed to connect to AMQP broker: " + err.Error())
 	}
-	defer conn.Close()
+	defer c.Close()
 
-	ch, err := conn.Channel()
+	ch, err := c.connection.Channel()
 	if err != nil {
 		return errors.New("failed to open a channel: " + err.Error())
 	}
